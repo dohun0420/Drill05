7c033b861e508ccc5d376aa378f0f5a5adc3ca98
dohun0420 <154303870+dohun0420@users.noreply.github.com>
2024-09-30 14:07:09 +0900

    캐릭터 무브 구현

--- Lecture05_Animation/character_moves.py
+++ Lecture05_Animation/character_moves.py
@@ -5,7 +5,12 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-# 여기를 채우세요.
+for x in range(0, 800, 5):
+    clear_canvas()  # 백버퍼를 지운다
+    grass.draw(400, 30) # 백버퍼에 그린다
+    character.draw(x, 90)
+    update_canvas() # 두개의 버퍼를 교환
+    delay(0.01)
 
 close_canvas()
 
