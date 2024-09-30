bad30e596dc521d7e1dc5aac363d043aff8f5209
dohun0420 <154303870+dohun0420@users.noreply.github.com>
2024-09-30 14:51:48 +0900

    캐릭터 런 esc 구현
    esc 누를시 종료

--- Lecture06_HandlingInputs/character_runs_esc.py
+++ Lecture06_HandlingInputs/character_runs_esc.py
@@ -4,15 +4,20 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-
-# fill here
-
-
+running = True
 
 def handle_events():
-    # fill here
-    pass
 
+    global running  # global이 없을 시 위의 running과 아래 running은 다른 취급
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False # global 없을 시 running : 지역변수
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+    events = get_events
+    pass
 
 frame = 0
 for x in range(0, 800, 5):
@@ -22,12 +27,11 @@ for x in range(0, 800, 5):
     character.clip_draw(frame * 100, 100, 100, 100, x, 90)
     update_canvas()
 
-    # fill here
-
-
+    handle_events()
+    if not running: # loop 제어변수
+        break
 
     frame = (frame + 1) % 8
     delay(0.05)
-
 
 close_canvas()
